cef0f875d1800a9dc563d4a65365bd2bde592705
adammalpass <adammalpass@hotmail.co.uk>
2017-07-29 09:20:57 +0900

    feat: Built out call-back function

--- src/sensor_stick/scripts/object_recognition.py
+++ src/sensor_stick/scripts/object_recognition.py
@@ -129,18 +129,45 @@ def pcl_callback(pcl_msg):
 # Exercise-3 TODOs: 
 
     # Classify the clusters! (loop through each detected cluster one at a time)
+    detected_objects_labels = []
+    detected_objects = []
 
+    for index, pts_list in enumerate(cluster_indices):
         # Grab the points for the cluster
+        pcl_cluster = cloud_out.extract(pts_list)
 
-        # Compute the associated feature vector
+        # TODO: convert the cluster from pcl to ROS using helper function
+        pcl_cluster_ros = pcl_to_ros(pcl_cluster)
 
-        # Make the prediction
+        # Extract histogram features
+        # TODO: complete this step just as you did before in capture_features.py
+        chists = compute_color_histograms(pcl_cluster_ros, using_hsv=True)
+        normals = get_normals(pcl_cluster_ros)
+        nhists = compute_normal_histograms(normals)
+        feature = np.concatenate((chists, nhists))
+
+        # Make the prediction, retrieve the label for the result
+        # and add it to detected_objects_labels list
+        prediction = clf.predict(scaler.transform(feature.reshape(1,-1)))
+        label = encoder.inverse_transform(prediction)[0]
+        detected_objects_labels.append(label)
 
         # Publish a label into RViz
+        label_pos = list(white_cloud[pts_list[0]])
+        label_pos[2] += .4
+        object_markers_pub.publish(make_label(label,label_pos, index))
 
         # Add the detected object to the list of detected objects.
+        do = DetectedObject()
+        do.label = label
+        do.cloud = ros_cluster
+        detected_objects.append(do)
+
+    rospy.loginfo('Detected {} objects: {}'.format(len(detected_objects_labels), detected_objects_labels))
 
     # Publish the list of detected objects
+    # This is the output you'll need to complete the upcoming project!
+    detected_objects_pub.publish(detected_objects)
 
 if __name__ == '__main__':
 
